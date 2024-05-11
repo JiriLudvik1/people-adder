4f4c1758f18736b42762a9d34352845328140907
jiriLu <jirka.ludvik1@gmail.com>
2024-05-11 21:08:53 +0200

    Extracted confirmed users to separate component

---
+++ src/components/ConfirmedUsers.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="confirmed-users-container">
+    <div class="confirmed-users-header"><h2>Confirmed Users</h2></div>
+        <div class="confirmed-users">
+            <div v-for="(user, index) in confirmedUsers" :key="index">
+                <img :src="user.picture.large" alt="User picture">
+                <p>{{ user.name.first }} {{ user.name.last }}</p>
+                <p>Rating: {{ user.rating }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { RatedUser } from '../types/types'
+
+@Component
+export default class ConfirmedUsers extends Vue {
+    confirmedUsers: RatedUser[] = []
+
+    addConfirmedUser (user: RatedUser) {
+      this.confirmedUsers.push(user)
+    }
+}
+</script>
+
+<style>
+.confirmed-users-container {
+    position: relative;
+    border: 1px solid #ddd; /* thin, light-colored border */
+    border-radius: 10px; /* rounded corners */
+    padding: 20px; /* some padding */
+    margin: 20px 0; /* some margin */
+  }
+
+  .confirmed-users-header {
+    position: absolute;
+    top: 0;
+    z-index: 1;
+    width: 100%; /* ensure the header takes up the full width of the container */
+    display: flex; /* use flexbox to center the text */
+    justify-content: center; /* center the text horizontally */
+    align-items: center; /* center the text vertically */
+  }
+
+  .confirmed-users {
+    display: flex;
+    justify-content: space-around;
+    align-content: space-around;
+    flex-wrap: wrap;
+    margin-top: 40px;
+    position: relative;
+    z-index: 0;
+  }
+  .confirmed-users img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+</style>
